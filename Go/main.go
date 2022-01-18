a43ba77fe9669ad2c3cf00cc54161ba4d88538d3
recs12 <slimane.rechdi@gmail.com>
2022-01-18 23:14:44 +0100

    remove commas from comments

--- Go/main.go
+++ Go/main.go
@@ -27,6 +27,7 @@ var Headers string = "path;part_id;number_bends;time_for_bends;with_adapter_bnc;
 var TagsRegx *regexp.Regexp = regexp.MustCompile("BEGIN_BIEGETEILSTAMM" + `((.|\n)*)` + "ENDE_BIEGETEILSTAMM")
 var TagsFields *regexp.Regexp = regexp.MustCompile("ZA,DA,1" + `((.|\n)*)` + `(^C$\s)*`)
 var TagsSubFields *regexp.Regexp = regexp.MustCompile("DA," + `((.|\n)*\d{1,2})` + `(.|\n)*`) // from DA to the end of the file.
+var commas *regexp.Regexp = regexp.MustCompile(`([^0-9,']),([^0-9,'])`)
 
 func createCsvAndHeaders(fileName string) {
 	// Create a csv file in the same location of the script.
@@ -51,17 +52,17 @@ func appendCsv(fileName, path, idNumber, bendsNumber, bendsTime, hasAdapter stri
 
 func getContentBetween(content string, regx regexp.Regexp) string {
 
-	var modified_content string = ""
+	var bucket string = ""
 	matches := regx.FindAllStringSubmatch(content, -1)
 	for _, v := range matches {
-		modified_content = modified_content + v[1]
+		bucket = bucket + v[1]
 	}
-	return modified_content
+	return bucket
 }
 
-func findAdapterModufixInBnc(paragraph, element string) string {
-	i := strings.Contains(paragraph, element)
-	return strconv.FormatBool(i)
+func findAdapterModufixInBnc(text, keyWord string) string {
+	isAdapter := strings.Contains(text, keyWord)
+	return strconv.FormatBool(isAdapter)
 }
 
 func getCommentsFromFile(pathBNCFile string) ([3]string, error) {
@@ -96,15 +97,23 @@ func getCommentsFromFile(pathBNCFile string) ([3]string, error) {
 
 	//CLEANING QUOTATION MARKS & COMMA
 	fieldsSepCleaned2 := strings.ReplaceAll(fieldsSepCleaned, "',  '", "','")
-
 	fieldsSepCleaned3 := strings.ReplaceAll(fieldsSepCleaned2, ",  ", ",")
 
+	//REMOVE COMMA FROM COMMENTS
+	withoutCommaInComment := commas.ReplaceAllString(fieldsSepCleaned3, "$1")
+
 	//SPLIT TO ARRAY
-	fieldsSequence := strings.Split(fieldsSepCleaned3, ",")
+	fieldsSequence := strings.Split(withoutCommaInComment, ",")
+
+	var sequence []string
+	for _, i := range fieldsSequence {
+		sequence = append(sequence, strings.TrimSpace(i))
+	}
+
 	var colData [3]string
-	if len(fieldsSequence) > 18 {
-		colData[0] = strings.TrimSpace(fieldsSequence[18])
-		colData[1] = strings.TrimSpace(fieldsSequence[21])
+	if len(sequence) > 18 {
+		colData[0] = strings.TrimSpace(sequence[18])
+		colData[1] = strings.TrimSpace(sequence[21])
 		colData[2] = hasAdapter
 		return colData, nil
 
@@ -128,8 +137,8 @@ func main() {
 	LogFileName := os.Args[1]
 	SearchDir := os.Args[2]
 
-	fmt.Println(LogFileName)
-	fmt.Println(SearchDir)
+	fmt.Println("CSV Name:", LogFileName)
+	fmt.Println("Target Folder:", SearchDir)
 	start := time.Now()
 
 	fileList := make([]string, 0)
@@ -151,7 +160,7 @@ func main() {
 		// skip files that are unvalide because of the commas in the comments
 		bendData, err := getCommentsFromFile(bncPath)
 		if err != nil || bendData[1] == "" {
-			appendFile(LogFileName, bncPath, [3]string{"scraping failed", "scraping failed", "scraping failed"})
+			appendFile(LogFileName, bncPath, [3]string{"failed", "failed", "failed"})
 		} else {
 			appendFile(LogFileName, bncPath, bendData)
 		}
